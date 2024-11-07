8a326372b3674436753138946280e57804a76da9
Hengsheng Li <hengsheng.li@tum.de>
2024-11-07 02:35:23 +0100

    2b finished

--- SystemC_Lab_Files/ex_2b/fifo_1b.cpp
+++ SystemC_Lab_Files/ex_2b/fifo_1b.cpp
@@ -4,10 +4,8 @@
 fifo_1b::fifo_1b(sc_module_name name, unsigned int fifo_size) : fifo_size(fifo_size) {
 	// ############# COMPLETE THE FOLLOWING SECTION ############# //
 	// register process
-	SC_THREAD(write_fifo);
-    sensitive << clk.pos();
-    SC_THREAD(read_fifo);
-    sensitive << clk.pos();
+	SC_METHOD(read_write_fifo);
+	sensitive << clk.pos();
 	// ####################### UP TO HERE ####################### //
 
 	// initialization of member variables/ports
@@ -19,13 +17,48 @@ fifo_1b::fifo_1b(sc_module_name name, unsigned int fifo_size) : fifo_size(fifo_s
 	// ############# COMPLETE THE FOLLOWING SECTION ############# //
 	// initialize output ports
 	full.initialize(false);
-    valid.initialize(true);
+	valid.initialize(false);
 	d_out.initialize(-1);
 	// ####################### UP TO HERE ####################### //
 }
 
 void fifo_1b::read_write_fifo() {
 	// ############# COMPLETE THE FOLLOWING SECTION ############# //
+	bool write = wr_en.read() && !full.read() && fill_level > 0;
+	bool read = rd_en.read() && valid.read();
+	bool write_through = wr_en.read() && fill_level == 0;
+	bool update_out = rd_en.read() && valid.read() && fill_level > 1;
+
+	if (write_through) {
+		d_out.write(d_in.read());
+		valid.write(true);
+		fill_level++;
+		cout << "@" << sc_time_stamp() << " Write through: " << (int)d_in.read() << endl;
+	}
+	if (update_out) {
+		d_out.write(fifo_data[rd_ptr.read()]);
+		cout << "@" << sc_time_stamp() << " Update out: " << (int)d_out.read() << " from position " << rd_ptr.read() << endl;
+	}
+	if (write) {
+		fifo_data[wr_ptr.read()] = d_in.read();
+		wr_ptr.write((wr_ptr.read() + 1) % fifo_size);
+		fill_level++;
+		cout << "@" << sc_time_stamp() << " Written: " << (int)d_in.read() << " at position " << wr_ptr.read() << endl;
+	}
+	if (read) {
+		if (fill_level == 1) {
+			fill_level = 0;
+			valid.write(false);
+		}
+		else {
+			rd_ptr.write((rd_ptr.read() + 1) % fifo_size);
+			fill_level--;
+		}
+		cout << "@" << sc_time_stamp() << " Read: " << (int)d_out.read() << " from position " << rd_ptr.read() << endl;
+	}
+
+	full.write(fill_level == (fifo_size + 1));
+	cout << "@" << sc_time_stamp() << " Fill level: " << fill_level << endl;
 
 	// ####################### UP TO HERE ####################### //
 }
--- SystemC_Lab_Files/ex_2b/main.cpp
+++ SystemC_Lab_Files/ex_2b/main.cpp
@@ -11,13 +11,52 @@ int sc_main(int argc, char *argv[]) {
 	// ############# COMPLETE THE FOLLOWING SECTION ############# //
 	// fill in the required commands to instantiate and connect producer, fifo,
 	// and consumer
+	sc_signal<unsigned char> d_in;
+	sc_signal<unsigned char> d_out;
+	sc_signal<bool> wr_en;
+	sc_signal<bool> rd_en;
+	sc_signal<bool> full;
+	sc_signal<bool> valid;
 
+    // Instantiate modules
+    producer prod("Producer");
+    consumer cons("Consumer");
+    fifo_1b fifo("FIFO", 5);
+
+    // Connect ports
+	prod.clk(clk);
+	prod.d_out(d_in);
+	prod.wr_en(wr_en);
+	prod.full(full);
+
+	cons.clk(clk);
+	cons.d_in(d_out);
+	cons.rd_en(rd_en);
+	cons.valid(valid);
+
+	fifo.clk(clk);
+	fifo.d_in(d_in);
+	fifo.wr_en(wr_en);
+	fifo.full(full);
+	fifo.d_out(d_out);
+	fifo.rd_en(rd_en);
+	fifo.valid(valid);
 	// ####################### UP TO HERE ####################### //
 
 	// ############# COMPLETE THE FOLLOWING SECTION ############# //
 	// fill in code to generate traces that can be used to observe the
 	// functionality of the model with the waveform viewer gtkwave
-
+	sc_trace_file *tf = sc_create_vcd_trace_file("fifo_1b");
+	sc_trace(tf, clk, "clk");
+	sc_trace(tf, rd_en, "rd_en");
+	sc_trace(tf, cons.fetch, "fetch");
+	sc_trace(tf, valid, "fifo_valid");
+	sc_trace(tf, fifo.fill_level, "fifo_fill_level");
+	sc_trace(tf, full, "fifo_full");
+	sc_trace(tf, d_in, "fifo_in");
+	sc_trace(tf, d_out, "fifo_out");
+	sc_trace(tf, prod.send, "send");
+	sc_trace(tf, wr_en, "wr_en");	
 	// ####################### UP TO HERE ####################### //
 
 	sc_time sim_dur = sc_time(5000, SC_NS);
