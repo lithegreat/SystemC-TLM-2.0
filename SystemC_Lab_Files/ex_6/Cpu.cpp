41e36784f134c85c5921a256155aeae86c95ed9f
Hengsheng Li <hengsheng.li@tum.de>
2024-12-05 17:11:00 +0100

    6 with error, implement packet transaction handling and error checks in Cpu class

--- SystemC_Lab_Files/ex_6/Cpu.cpp
+++ SystemC_Lab_Files/ex_6/Cpu.cpp
@@ -10,25 +10,77 @@ void Cpu::processor_thread(void) {
 	while(true) {
 		// ############# COMPLETE THE FOLLOWING SECTION ############# //
 		// read new packet descriptor
-
+		wait(packetReceived_interrupt.posedge_event());
 		// ####################### UP TO HERE ####################### //
 
 		// ############# COMPLETE THE FOLLOWING SECTION ############# //
 		// Forward the packet descriptor to an arbitrary port
 
+		// Finally, send the packet descriptor to a randomly selected output queue. For this,
+		// execute a further appropriate transaction as you have just done (i.e., set up the
+		// transaction, initialize phase and delay, call function, verify …).
+
+		// OUTPUT_0_ADDRESS 0x20000000 write only
+		// OUTPUT_1_ADDRESS 0x30000000 write only
+		// OUTPUT_2_ADDRESS 0x40000000 write only
+		// OUTPUT_3_ADDRESS 0x50000000 write only
+
+		soc_address_t output_address = 0x20000000 + (rand() % 4) * 0x10000000;
+		startTransaction(TLM_WRITE_COMMAND, output_address, (unsigned char *) &m_packet_descriptor, sizeof(packet_descriptor));
 		// ####################### UP TO HERE ####################### //
 	}
 }
 
 // ############# COMPLETE THE FOLLOWING SECTION ############# //
 // startTransaction
+void Cpu::startTransaction(tlm_command command, soc_address_t address, unsigned char *data, unsigned int dataSize) {
+	payload.set_command(command);
+	payload.set_address(address);
+	payload.set_data_ptr(data);
+	payload.set_data_length(dataSize);
 
+	tlm_phase phase = BEGIN_REQ;
+	sc_time delay_time = SC_ZERO_TIME;
+	tlm_sync_enum resp = initiator_socket->nb_transport_fw(payload, phase, delay_time);
+	
+	cout << "Waiting for the transaction to finish." << endl;
+	wait(transactionFinished_event);
+
+	if (resp != TLM_UPDATED) {
+		cout << "Error: " << "Transaction not updated correctly." << endl;
+		exit(1);
+	}
+
+	if (phase != END_REQ) {
+		cout << "Error: " << "Transaction phase not updated correctly." << endl;
+		exit(1);
+	}
+
+	if (payload.get_response_status() != TLM_OK_RESPONSE || payload.is_response_error()) {
+		cout << "Error: " << "Transaction not completed successfully." << endl;
+		exit(1);
+	}
+
+}
 // ####################### UP TO HERE ####################### //
 
 // nb_transport_bw: implementation of the backward path callback
 tlm_sync_enum Cpu::nb_transport_bw(tlm_generic_payload& transaction,
 		tlm_phase& phase, sc_time& delay_time) {
 	// ############# COMPLETE THE FOLLOWING SECTION ############# //
+
+	// check whether the phase is set appropriately when nb_transport_bw is called.
+	if (phase != END_REQ) {
+		cout << "Error: " << "Phase not set correctly." << endl;
+		exit(1);
+	}
+	// The bus transfer should take one bus cycle (CLK_CYCLE_BUS). Update the delay
+	delay_time += sc_time(CLK_CYCLE_BUS);
+
+	transactionFinished_event.notify(delay_time);
+	phase = BEGIN_RESP;
+	return TLM_COMPLETED;
+
 
 	// ####################### UP TO HERE ####################### //
 }
